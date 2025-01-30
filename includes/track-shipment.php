378c3734764b18fe0120aadf1726fa5b88d857b3
ijidev <iheanyichukwui94@gmail.com>
2025-01-30 09:08:34 +0100

    first commit.

---
+++ includes/track-shipment.php
@@ -0,0 +1,32 @@
+<?php
+
+  echo $_POST['tracking_code'] ;
+
+  $tracking_code = $_POST['tracking_code'];
+  $curl = curl_init();
+
+  if (!$curl) {
+    echo " Error retriving tracking info" ;
+  } else {
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://api-test.dhl.com/track/shipments?trackingNumber='.$tracking_code,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'DHL-API-Key: DwuAtkLvmQ4gj7jXOTsO3UFjeOPA6Pec'
+      ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+  }
+  
+
+?>
